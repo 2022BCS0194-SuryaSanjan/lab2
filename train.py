5e084cea187e70ba5c1d4e8a96c494a85017ea1a
2022BCS0194-SuryaSanjan <surya_sanjan@DESKTOP-J58HDC8.>
2026-01-09 16:20:16 +0000

    Experiment 2: Ridge Regression alpha=1.0

--- train.py
+++ train.py
@@ -43,7 +43,8 @@ X_test = scaler.transform(X_test)
 # -------------------------------
 # Model Training
 # -------------------------------
-model = LinearRegression()
+from sklearn.linear_model import Ridge
+model = Ridge(alpha=1.0)
 model.fit(X_train, y_train)
 
 # -------------------------------

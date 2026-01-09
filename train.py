ec01fc7082761821c35f74f0770109ec415922ee
2022BCS0194-SuryaSanjan <surya_sanjan@DESKTOP-J58HDC8.>
2026-01-09 16:23:27 +0000

    Experiment 4: Linear Regression without scaling

--- train.py
+++ train.py
@@ -30,15 +30,16 @@ y = df["quality"]
 # Train-Test Split
 # -------------------------------
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=42
+    X, y, test_size=0.2, random_state=42
 )
 
 # -------------------------------
 # Preprocessing
 # -------------------------------
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+# test 4 no scalling
+# scaler = StandardScaler()
+# X_train = scaler.fit_transform(X_train)
+# X_test = scaler.transform(X_test)
 
 # -------------------------------
 # Model Training

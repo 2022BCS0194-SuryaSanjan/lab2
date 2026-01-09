3be66ba6bc02a7998ea6ce74a3e50f48f0ee3aa3
2022BCS0194-SuryaSanjan <surya_sanjan@DESKTOP-J58HDC8.>
2026-01-09 16:21:48 +0000

    Experiment 3: Linear Regression with 70-30 split

--- train.py
+++ train.py
@@ -30,7 +30,7 @@ y = df["quality"]
 # Train-Test Split
 # -------------------------------
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X, y, test_size=0.3, random_state=42
 )
 
 # -------------------------------
